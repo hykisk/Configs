016bc0507331210d298d896a67b8c5da9f1bfcdf
hykisk <isshiki.hiroyuki@gmail.com>
2018-11-11 01:12:58 +0900

    [move] '.git-prompt.sh' to Git

--- createln.sh
+++ createln.sh
@@ -3,14 +3,19 @@
 echo "############ BEFORE ##########################"
 ls -al -d `find ~ -maxdepth 1 -name ".*"`
 
+echo "############ Backup ##########################"
+cp -p ~/.bashrc ~/.bashrc.org
+cp -p ~/.profile ~/.profile.org
+
 echo "############ Create symbolic link ############"
+mkdir -p ~/bin
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.bash_profile ~/.bash_profile
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.bashrc ~/.bashrc
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.eslintrc.json ~/.eslintrc.json
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.gitconfig ~/.gitconfig
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.prettierrc.js ~/.prettierrc.js
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/bin/winlxss.sh ~/bin/winlxss.sh
-ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/bin/.git-prompt.sh ~/.git-prompt.sh
+ln -sf /mnt/c/work/Configs/Git/.git-prompt.sh ~/.git-prompt.sh
 ln -sf /mnt/c/work/Configs/Vim/.vimrc ~/.vimrc
 ln -sf /mnt/c/work/Configs/Vim/cheatsheet.md ~/.cheatsheet.md
 ln -sf /mnt/c/work/Configs/ls-Color/ ~/.dircolors-solarized
@@ -20,4 +25,3 @@ ln -sf /usr/local/bin/vim /usr/bin/vi
 ln -sf ~/bin/winlxss.sh /usr/bin/winlxss
 echo "############ AFTER ###########################"
 ls -al -d `find ~ -maxdepth 1 -name ".*"`
-
