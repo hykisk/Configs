513ee685728e1184575725b8cca73fbc65447553
hykisk <isshiki.hiroyuki@gmail.com>
2018-11-11 01:52:13 +0900

    [move] configs WSL to Vim

--- createln.sh
+++ createln.sh
@@ -11,13 +11,13 @@ echo "############ Create symbolic link ############"
 mkdir -p ~/bin
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.bash_profile ~/.bash_profile
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.bashrc ~/.bashrc
-ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.eslintrc.json ~/.eslintrc.json
-ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.gitconfig ~/.gitconfig
-ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.prettierrc.js ~/.prettierrc.js
 ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/bin/winlxss.sh ~/bin/winlxss.sh
-ln -sf /mnt/c/work/Configs/Git/.git-prompt.sh ~/.git-prompt.sh
-ln -sf /mnt/c/work/Configs/Vim/.vimrc ~/.vimrc
+ln -sf /mnt/c/work/Configs/Windows-Subsystem-for-Linux/.gitconfig ~/.gitconfig
+ln -sf /mnt/c/work/Configs/Vim/.eslintrc.json ~/.eslintrc.json
+ln -sf /mnt/c/work/Configs/Vim/.prettierrc.js ~/.prettierrc.js
 ln -sf /mnt/c/work/Configs/Vim/cheatsheet.md ~/.cheatsheet.md
+ln -sf /mnt/c/work/Configs/Vim/.vimrc ~/.vimrc
+ln -sf /mnt/c/work/Configs/Git/.git-prompt.sh ~/.git-prompt.sh
 ln -sf /mnt/c/work/Configs/ls-Color/ ~/.dircolors-solarized
 ln -sf /mnt/c/work/Configs/tmux/.tmux.conf ~/.tmux.conf
 
