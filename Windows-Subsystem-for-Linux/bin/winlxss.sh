013c12c65d5cd0eb6fab1b1d86c6a99090a61094
hykisk <isshiki.hiroyuki@gmail.com>
2019-11-15 18:10:12 +0900

    Update winlxss. change wsl file-system path

--- Windows-Subsystem-for-Linux/bin/winlxss.sh
+++ Windows-Subsystem-for-Linux/bin/winlxss.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-windir='C:/Users/hykisk/AppData/Local/lxss'
+windir='C:/Users/hykisk/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs'
+
 BoWpath=$(readlink -f $1)
 cmd.exe /c start $windir$BoWpath
 
